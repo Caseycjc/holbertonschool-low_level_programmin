b30161dc7ccea4eaf4c1313cea4b0a2235b32f62
root <knightlaggia@gmail.com>
2022-09-15 09:32:28 -0700

    c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,6 @@
 #include <stdio.h>
-/**
- * main - Entry point
- * Return: Always 0 (Success)
- */
-int main(void)
-{
-        printf("\"Programming is like building a multilingual puzzle");
-        return (0);
+
+int main (){
+   print f ("with proper grammar, but the outcome is a piece of art,);
+   return 0;
+}
