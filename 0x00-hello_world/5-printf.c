f432b5ffcc4160bb97b9495386aeeb81ef91337c
root <knightlaggia@gmail.com>
2022-09-15 09:35:21 -0700

    c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,4 @@
 #include <stdio.h>
 
-int main (){
-   print f ("with proper grammar, but the outcome is a piece of art,);
-   return 0;
-}
+int printf ("with proper grammar, but the outcome is a piece of art,")
+return 0;
