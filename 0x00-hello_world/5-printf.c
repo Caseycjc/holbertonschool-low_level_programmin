d9020446ca2401036cf0ae107a67ce8ce23cd027
root <knightlaggia@gmail.com>
2022-09-15 11:24:02 -0700

    c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 int main()
 {
-	printf("with proper grammar, but the outcome is a piece of art,")
+	// Displays the string inside quotations
+	printf("with proper grammar, but the outcome is a piece of art,");
 	return 0;
 }
