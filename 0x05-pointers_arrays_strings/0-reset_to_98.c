d391d0effd92067dca1fb66586710870f7ce85b1
root <knightlaggia@gmail.com>
2022-09-26 11:29:06 -0700

    pointers

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - updates the value to 98
+ * @n : n is the pointer of n and return value to 98
+ * Return: Always 0.
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
