868730d7df4d527a09e720125c6bb07bb75d7872
root <knightlaggia@gmail.com>
2022-09-27 12:26:55 -0700

    pointers

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _puts - Fuction that prints a string followed by a new line
+ * @str: validate the character
+ * Return: Always 0.
+ */
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0' ; i++)
+	{
+		-putchar(str[1]);
+	}
+	_putchar('\n');
+}
