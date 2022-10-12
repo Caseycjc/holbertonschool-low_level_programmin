61e3d7f4a623eb1423d40db52ff2eb9e483b82e4
root <knightlaggia@gmail.com>
2022-10-11 20:51:35 -0700

    argc

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,21 @@
+#include "stdio.h"
+
+/**
+ * main - entry point
+ * @argc: counts the number of parameters that go into main
+ * @argv: pointer of array of pointers containing strings entering main
+ * Return: Always 0.
+ */
+int main(int argc, char **argv)
+{
+	int i;
+
+	if (argc > 0)
+	{
+		for (i = 0; i <argc; i++)
+		{
+			printf("%s\n", argc[i]);
+		}
+	}
+	return (0);
+}
