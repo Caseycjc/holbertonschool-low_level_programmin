b891cc77bd514944c6257ceeffd41eec5c980a29
root <knightlaggia@gmail.com>
2022-10-11 20:26:06 -0700

    argc

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include "stdio.h"
+
+/**
+ * main - entry point
+ * @argc: counts the number of parameters that go into main
+ * @argv: pointer of array of pointers containing strings entering main
+ * Return: Always 0.
+ */
+int main(int argc, char **argv)
+{
+	if (argc > 0)
+		printf("%s\n", argv[0]);
+	return (0);
+}
